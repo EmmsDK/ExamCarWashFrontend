bb322839c085607c8936d678dbd367fbe54beae4
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2023-06-12 09:26:04 +0200

    ready

---
+++ src/Setting.js
@@ -0,0 +1,14 @@
+export const
+    URL = 'http://localhost:8080/examtemplate',
+    AnimalUrl = "http://localhost:8080/exam/api/animal/",
+    FavAnimalURL = "http://localhost:8080/exam/api/animal/favorite",
+    RemoveFavAnimalURL = "http://localhost:8080/exam/api/animal/remove",
+    AllFavAnimalURL = "http://localhost:8080/exam/api/animal/getfavorites",
+    homemadeURL = "http://localhost:3000/facts",
+    FavFactURL = "http://localhost:8080/sys/exam/fact/favorite",
+
+    FactUrl = "http://localhost:3000/facts",
+    FactAPIURL = "http://random-facts1.p.rapidapi.com/fact/search",
+    AnimalAPIURL = "http://rapidapi.com/apininjas/api/animals-by-api-ninjas/";
+
+

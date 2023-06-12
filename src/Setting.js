7ee4b6702c22d477974d799f7aa8198f051851fa
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2023-06-12 11:43:35 +0200

    package.json

--- src/Setting.js
+++ src/Setting.js
@@ -1,14 +1,6 @@
 export const
-    URL = 'http://localhost:8080/examtemplate',
-    AnimalUrl = "http://localhost:8080/exam/api/animal/",
-    FavAnimalURL = "http://localhost:8080/exam/api/animal/favorite",
-    RemoveFavAnimalURL = "http://localhost:8080/exam/api/animal/remove",
-    AllFavAnimalURL = "http://localhost:8080/exam/api/animal/getfavorites",
-    homemadeURL = "http://localhost:3000/facts",
-    FavFactURL = "http://localhost:8080/sys/exam/fact/favorite",
+    URL = 'http://localhost:8080/carwash',
+    WashingAssistantURL = 'http://localhost:8080/carwash/api/washing-assistant/';
 
-    FactUrl = "http://localhost:3000/facts",
-    FactAPIURL = "http://random-facts1.p.rapidapi.com/fact/search",
-    AnimalAPIURL = "http://rapidapi.com/apininjas/api/animals-by-api-ninjas/";
 
 
